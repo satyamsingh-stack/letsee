7f2235661e5392021e55101bc7d798b69d50c6f3
Satyam Singh <>
2023-09-23 23:56:06 +0530

    oops

--- front-end/src/component/Nav.js
+++ front-end/src/component/Nav.js
@@ -17,6 +17,7 @@ const Nav = () => {
 
             {auth ? <ul className='Nav-ul'>
                 <li><Link to="/"> Home</Link></li>
+                <li><Link to="/oops">Oops</Link></li>
                 <li><Link to="/Contact"> Contact Us</Link></li>
                 <li><Link to="/About"> About Us</Link></li>
                 <li><Link onClick={logout} to="/signup">Logout ({JSON.parse(auth).name})</Link></li>
